e76a7e0ce7f25f37f6bc29fe895cc40fbe7cf9a1
Salah-eddine <chnitifa.salah@gmail.com>
2023-11-02 14:03:02 +0100

    Variables : Add comment in tp1

--- variables/tp1.py
+++ variables/tp1.py
@@ -1,3 +1,6 @@
+# -1- Créer un nouveau projet sur PyCharm, et saisir le script suivant et
+#     l’enregistrer sous le nom tp1.py
+
 a = 5
 print ("a = ", a)
 
@@ -13,3 +16,15 @@ print ("type de a: ",type(a))
 print ("type de b: ",type(b))
 print ("type de c: ",type(c))
 print ("type de texte: ",type(texte))
+
+# result :
+# a =  5
+# b =  5.5
+# c =  (5, 50, 14)
+# Mon texte
+# type de a:  <class 'int'>
+# type de b:  <class 'float'>
+# type de c:  <class 'tuple'>
+# type de texte:  <class 'str'>
+
+# Process finished with exit code 0
