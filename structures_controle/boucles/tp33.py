24ac380db664c1054b943b59f52b396bc1443e9a
Salah-eddine <chnitifa.salah@gmail.com>
2023-11-02 15:07:56 +0100

    Les Structures de contrôle : Les boucles tp 33

---
+++ structures_controle/boucles/tp33.py
@@ -0,0 +1,20 @@
+# -33- Écrire un programme, qui affiche 9 fois ”Je suis un bon programmeur en
+#      Python ” à l’aide de l’instruction for.
+
+# Utilisation de la boucle for pour afficher la chaîne 9 fois
+for i in range(9):
+    print("Je suis un bon programmeur en Python")
+
+# result :
+
+# Je suis un bon programmeur en Python
+# Je suis un bon programmeur en Python
+# Je suis un bon programmeur en Python
+# Je suis un bon programmeur en Python
+# Je suis un bon programmeur en Python
+# Je suis un bon programmeur en Python
+# Je suis un bon programmeur en Python
+# Je suis un bon programmeur en Python
+# Je suis un bon programmeur en Python
+#
+# Process finished with exit code 0
