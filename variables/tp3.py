1240f0c5c64cd8578c23974ddb597c6b826cff55
Salah-eddine <chnitifa.salah@gmail.com>
2023-11-02 14:10:43 +0100

    Variables : Add comment in tp3

--- variables/tp3.py
+++ variables/tp3.py
@@ -1,5 +1,8 @@
 import math
 
+# -3- Écrire un programme qui, à partir de la saisie d’un rayon et d’une hauteur,
+#     calcule le volume d’un cône droit :
+
 # Saisie du rayon et de la hauteur
 rayon = float(input("Entrez la valeur du rayon : "))
 hauteur = float(input("Entrez la valeur de la hauteur : "))
@@ -9,3 +12,11 @@ volume = (1/3) * math.pi * rayon**2 * hauteur
 
 # Affichage du volume
 print(f"Le volume du cône avec un rayon de {rayon} et une hauteur de {hauteur} est {volume}")
+
+# result :
+
+# Entrez la valeur du rayon : 4
+# \Entrez la valeur de la hauteur : 12
+# Le volume du cône avec un rayon de 4.0 et une hauteur de 12.0 est 201.06192982974676
+#
+# Process finished with exit code 0
