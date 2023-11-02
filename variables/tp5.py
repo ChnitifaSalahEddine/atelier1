abffbf3f65f4fe3c2b690bb6b08e17fb3be7225f
Salah-eddine <chnitifa.salah@gmail.com>
2023-11-02 14:39:42 +0100

    Variables : Add comment in tp5

--- variables/tp5.py
+++ variables/tp5.py
@@ -12,9 +12,14 @@ a=3
 # Affichage du type du résultat pour les deux instructions
 print("Le type de la variable a est :", type(a))
 
+# result :
+
 #   File "/Users/salaheddine/PycharmProjects/atelier1/variables/tp5.py", line 10
 #     a= =3
 #        ^
 # SyntaxError: invalid syntax
 #
 # Process finished with exit code 1
+
+# Si vous essayez d'exécuter le code avec a= =3,
+# vous obtiendrez une erreur de syntaxe indiquant que l'opérateur n'est pas reconnu.
