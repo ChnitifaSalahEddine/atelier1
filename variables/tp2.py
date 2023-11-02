91bf4f083a34298c53d43beda9bc9c407f418b49
Salah-eddine <chnitifa.salah@gmail.com>
2023-11-02 14:07:38 +0100

    Variables : Add comment in tp2

--- variables/tp2.py
+++ variables/tp2.py
@@ -1,13 +1,33 @@
-#a. Pour définir les variables et afficher leur type :
-# Définition des variables
+# -2-
+
+# a. Pour définir les variables et afficher leur type :
+
+#Définition des variables
 texte_variable = "Bonjour"
 nombre_entier_variable = 7
 nombre_decimal_variable = 3.14
 
-#b. Pour affecter les 3 variables dans une même ligne :
+# b. Pour affecter les 3 variables dans une même ligne :
+
 #texte_variable, nombre_entier_variable, nombre_decimal_variable = "Bonjour", 7, 3.14
 
 # Affichage des types
 print("Le type de la variable texte est :", type(texte_variable))
 print("Le type de la variable nombre_entier est :", type(nombre_entier_variable))
 print("Le type de la variable nombre_decimal est :", type(nombre_decimal_variable))
+
+# result :
+# a.
+# Le type de la variable texte est : <class 'str'>
+# Le type de la variable nombre_entier est : <class 'int'>
+# Le type de la variable nombre_decimal est : <class 'float'>
+#
+# Process finished with exit code 0
+
+# b.
+
+# Le type de la variable texte est : <class 'str'>
+# Le type de la variable nombre_entier est : <class 'int'>
+# Le type de la variable nombre_decimal est : <class 'float'>
+#
+# Process finished with exit code 0
