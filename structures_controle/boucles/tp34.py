a64ec52dedd96bcf595feb3fb5bb30974a04c81f
Salah-eddine <chnitifa.salah@gmail.com>
2023-11-02 15:08:04 +0100

    Les Structures de contrôle : Les boucles tp 34

---
+++ structures_controle/boucles/tp34.py
@@ -0,0 +1,25 @@
+# -34- Écrire un programme qui affiche la table de multiplications de « 2 » (de 0
+#      jusqu’à 10) en affichant : 2 * 0 = 0.
+
+# Affichage de la table de multiplication de 2 de 0 à 10
+nombre = 2
+for i in range(11):
+    resultat = nombre * i
+    print(f"{nombre} * {i} = {resultat}")
+
+# result :
+
+# 2 * 0 = 0
+# 2 * 1 = 2
+# 2 * 2 = 4
+# 2 * 3 = 6
+# 2 * 4 = 8
+# 2 * 5 = 10
+# 2 * 6 = 12
+# 2 * 7 = 14
+# 2 * 8 = 16
+# 2 * 9 = 18
+# 2 * 10 = 20
+#
+# Process finished with exit code 0
+
